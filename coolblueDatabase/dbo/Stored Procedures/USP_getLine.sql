26b3e8d5a28b221f20be007b423318c05a06d503
Michael Caditz <michael@spaworks.com>
2021-01-19 17:13:54 -0800

    alltrans

--- coolblueDatabase/dbo/Stored Procedures/USP_getLine.sql
+++ coolblueDatabase/dbo/Stored Procedures/USP_getLine.sql
@@ -13,9 +13,7 @@ CREATE PROCEDURE [dbo].[USP_getLine]
 AS
 BEGIN
 
-
-
-	-- SET NOCOUNT ON added to prevent extra result sets from
+-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
@@ -24,15 +22,22 @@ BEGIN
 	 SELECT distinct a.ID,a.dtCreateDate,a.nAccountID,a.dtTransDate,a.cNote,
 	 
 	 (select isnull(sum(s.nAmount),0) as "totalSplit" from dbo.split s where s.nLineID = a.ID
-	 and (s.nAccountID_C = @accountID or s.nAccountID_D = @accountID)
+	 and (c.bIsAll = 1 or(s.nAccountID_C = @accountID or s.nAccountID_D = @accountID))
 	 and (s.bDeleted is null or s.bDeleted=0) ) as "totalsplit",
 
+
+
+
 	  (select isnull(sum(s.nAmount_D),0) as "totalDr" from dbo.split s where s.nLineID = a.ID
-	   and (s.nAccountID_D = @accountID)
+	   and (c.bIsAll = 1 or (s.nAccountID_D = @accountID))
 	 and (s.bDeleted is null or s.bDeleted=0) ) as "totalDr",
 
+
+
+
+
 	  (select isnull(sum(s.nAmount_C),0) as "totalCr" from dbo.split s where s.nLineID = a.ID
-	   and (s.nAccountID_C = @accountID )
+	   and (c.bIsAll = 1 or(s.nAccountID_C = @accountID ))
 	 and (s.bDeleted is null or s.bDeleted=0) ) as "totalCr"
 
 
