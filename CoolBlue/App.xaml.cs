45041c079eac2bb32746dfa4cffb8997150064e7
Michael Caditz <michael@spaworks.com>
2021-02-01 12:37:20 -0800

    startup

--- CoolBlue/App.xaml.cs
+++ CoolBlue/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using coolBlue;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,42 @@ namespace CoolBlue
     /// </summary>
     public partial class App : Application
     {
+        void App_Startup(object sender, StartupEventArgs e)
+        {
+
+            //string message = "Start program?";
+            //string caption = "CoolBlue";
+
+            //MessageBoxButton buttons = MessageBoxButton.OKCancel;
+            //MessageBoxImage icon = MessageBoxImage.Information;
+            //MessageBoxResult defaultResult = MessageBoxResult.OK;
+            //MessageBoxOptions options = MessageBoxOptions.None;
+            
+            //MessageBoxResult result = MessageBox.Show(message, caption, buttons, icon, defaultResult, options);
+
+            //if (result == MessageBoxResult.Cancel)
+            //{
+
+            //    // Closes the parent form.
+
+            //    //this.Close();
+
+            //    System.Windows.Application.Current.Shutdown();
+            //    return;
+            //}
+
+            //else
+            //{
+
+
+
+            //}
+
+
+
+                MainWindow window = new MainWindow();
+                window.Show();
+            
+        }
     }
 }
