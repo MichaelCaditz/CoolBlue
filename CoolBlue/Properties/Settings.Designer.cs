68e4356546cb7220cef5b54431914dae521d4c7b
Michael Caditz <michael@highmountainweb.com>
2022-07-07 23:25:33 -0700

    new server

--- CoolBlue/Properties/Settings.Designer.cs
+++ CoolBlue/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace coolBlue.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -26,7 +26,7 @@ namespace coolBlue.Properties {
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
-        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=SAGER10\\SAGER2017;Initial Catalog=coolblue;Integrated Security=True")]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=main3;Initial Catalog=coolblue;Integrated Security=True")]
         public string coolblueConnectionString {
             get {
                 return ((string)(this["coolblueConnectionString"]));
@@ -35,7 +35,7 @@ namespace coolBlue.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("SAGER10\\SAGER2017")]
+        [global::System.Configuration.DefaultSettingValueAttribute("main3")]
         public string server {
             get {
                 return ((string)(this["server"]));
